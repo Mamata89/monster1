e5a876d04276e9e73b1bfa1f323bf90a04912ab9
Mamata Moger <mamata.moger@gmail.com>
2020-12-08 21:00:18 +0530

    Updates

---
+++ precache-manifest.21674c6e044fc8247560f1bbd6c9d3ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33541f24e59f4fc038fbde0ce99b25e",
+    "url": "/Mamata89.github.io/monster1/index.html"
+  },
+  {
+    "revision": "41613955f32af47eed10",
+    "url": "/Mamata89.github.io/monster1/static/css/main.8459afe9.chunk.css"
+  },
+  {
+    "revision": "660f4d062e4822a1848f",
+    "url": "/Mamata89.github.io/monster1/static/js/2.62c2cd20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Mamata89.github.io/monster1/static/js/2.62c2cd20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41613955f32af47eed10",
+    "url": "/Mamata89.github.io/monster1/static/js/main.8d10a110.chunk.js"
+  },
+  {
+    "revision": "ac1a1f37d8c33f6f2e36",
+    "url": "/Mamata89.github.io/monster1/static/js/runtime-main.6a52dba0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Mamata89.github.io/monster1/static/media/logo.ee7cd8ed.svg"
+  }
+]);
